a470dc0b8d9bc22387c2749043203e7e75d9146e
Hyun Woo Park <phu54321@naver.com>
2019-12-06 03:14:53 +0900

    feat: re-navigate on onBeforeNavigate

--- source/background.js
+++ source/background.js
@@ -1,34 +1,25 @@
 import tldList from './tldList'
 
 const maybeNonTLDQuery = /^https?:\/\/([^/]+)\/$/
-browser.webNavigation.onCommitted.addListener((evt) => {
-  if (evt.frameId !== 0) {
-    return
-  }
+browser.webNavigation.onBeforeNavigate.addListener((evt) => {
+  // Ignore non-top-level navigation. e.g) iframe
+  if (evt.frameId !== 0) return
 
-  // Ignore non-omnibox-typed events
-  if (evt.transitionType !== 'typed') return
+  const tabId = evt.tabId
 
-  console.log('onCommitted', evt)
-
-  // Non-standard tld
+  // Non-standard tld test
   if (maybeNonTLDQuery.test(evt.url)) {
     const url = evt.url.match(maybeNonTLDQuery)[1]
     const segments = url.split('.')
     const tld = segments[segments.length - 1].toUpperCase()
 
     if (tldList.indexOf(tld) === -1) {
-      if (url.startsWith('www.')) return searchWith(url.substr(4))
-      else return searchWith(url)
+      if (url.startsWith('www.')) return searchWith(url.substr(4), tabId)
+      else return searchWith(url, tabId)
     }
   }
 })
 
-async function searchWith (query) {
-  const currentTabs = await browser.tabs.query({ active: true, currentWindow: true })
-  if (currentTabs.length !== 1) {
-    throw new Error('Invalid currentTabs: ' + currentTabs)
-  }
-  const currentTab = currentTabs[0]
-  browser.search.search({ query, tabId: currentTab.id })
+function searchWith (query, tabId) {
+  browser.search.search({ query, tabId })
 }
